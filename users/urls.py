6b58562a4532dcb9b2d33371c2b76eb700926c22
kidus <kidusefrem2@gmail.com>
2026-08-12 11:19:52 +0300

    chore: sort imports in users/urls.py to fix CI

--- users/urls.py
+++ users/urls.py
@@ -1,7 +1,7 @@
 from django.urls import path
 from rest_framework_simplejwt.views import TokenBlacklistView, TokenRefreshView
 
-from .views import RequestOTPView, UserProfileView, VerifyOTPView, UpdateProfileView
+from .views import RequestOTPView, UpdateProfileView, UserProfileView, VerifyOTPView
 
 urlpatterns = [
     # Notice we don't repeat '/api/v1/auth/' here, just the final part of the path
