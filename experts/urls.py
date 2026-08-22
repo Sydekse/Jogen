c6ac944599e2f54477e6a800ee86571786bd590a
Nehemiah <nehemiahmitiku@gmail.com>
2026-08-22 04:36:12 -0700

    fix: support trailing and non-trailing slashes for expert endpoints

--- experts/urls.py
+++ experts/urls.py
@@ -11,11 +11,18 @@ from .views import (
 urlpatterns = [
     path("", ExpertListView.as_view(), name="expert_list"),
     path("profile", ExpertProfileView.as_view(), name="expert_profile"),
+    path("profile/", ExpertProfileView.as_view()),
     path("availability", ExpertAvailabilityView.as_view(), name="expert_availability"),
+    path("availability/", ExpertAvailabilityView.as_view()),
     path("<uuid:expert_id>", ExpertDetailView.as_view(), name="expert_detail"),
+    path("<uuid:expert_id>/", ExpertDetailView.as_view()),
     path(
         "<uuid:expert_id>/availability",
         ExpertPublicAvailabilityView.as_view(),
         name="expert_public_availability",
+    ),
+    path(
+        "<uuid:expert_id>/availability/",
+        ExpertPublicAvailabilityView.as_view(),
     ),
 ]
